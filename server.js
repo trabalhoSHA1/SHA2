8eced29bb8fd7ebd853980629deda9fb3885daa1
davipetzold <davi17petzold@gmail.com>
2025-08-30 11:46:11 -0300

    alterações bd

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import Routeslib from "./src/lib/Routes.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.listen(port);
+
+console.log("rodando");
