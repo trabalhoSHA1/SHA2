1b783aa88800276762f11441c87106fbb62c0436
Luíza <luiza.otonivilhena@gmail.com>
2025-10-14 10:54:04 -0300

    alterações bd

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 import express from "express";
-import Routeslib from "./src/lib/Routes.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT;
 
 const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.post("/", (req, res) => {
+  users.push(req.body);
+  res.status(201).json(req.body);
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json(users);
+});
+
 
 app.listen(port);
 
