61c6fa6c893ae9c6d0bab7453cc5618297673af0
davipetzold <davi17petzold@gmail.com>
2025-11-11 05:41:30 -0300

    alterações bd

--- server.js
+++ server.js
@@ -1,34 +1,42 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 
-
 const prisma = new PrismaClient();
-
 
 const app = express();
 app.use(express.json());
 
-const users = [];
-
-app.post("/", async(req, res) => {
+app.post("/usuarios", async (req, res) => {
   await prisma.user.create({
-    data:{
+    data: {
       email: req.body.email,
       name: req.body.nome,
-      age: req.body.age
-    }
-  })
+      age: req.body.age,
+    },
+  });
   res.status(201).json(req.body);
 });
 
-app.get("/", (req, res) => {
+app.get("/usuarios", async (req, res) => {
+  const users = await prisma.user.findMany();
   res.status(200).json(users);
 });
 
+app.put("/usuarios/:id", async (req, res) => {
+  console.log(req);
+  //   await prisma.user.update({
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //     data: {
+  //       email: req.body.email,
+  //       name: req.body.nome,
+  //       age: req.body.age,
+  //     },
+  //   });
+  //   res.status(201).json(req.body);
+});
 
 app.listen(3000);
 
 console.log("rodando");
-
-// SHA
-// SHA@abc
